e3c7e6542fa023b703a7e3909367a3ae9a1fe769
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2019-03-06 15:43:37 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/styles'
+import { Button } from '@material-ui/core'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+const styledBy = (property, mapping) => props => mapping[props[property]];
+const useStyles = makeStyles(({ palette }) => ({
+  container: {
+    position: 'absolute',
+    top: '40%',
+    left: 'calc(50% - 100px)'
+  },
+  styledPaper: {
+    width: 200,
+    height: 200,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: 'white',
+
+    backgroundColor: styledBy("selected", {
+      true: palette.primary.main,
+      false: palette.secondary.main,
+    }),
   }
+}))
+
+function App() {
+  const [selected, setSelected] = useState(false)
+  const styles = useStyles({ selected })
+
+  return (
+    <div className={styles.container}>
+      <Button
+        onClick={_ => setSelected(!selected)}
+        className={styles.styledPaper}
+      >
+        {`${selected}`}
+      </Button>
+    </div> 
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './bootstrap'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { createMuiTheme } from '@material-ui/core'
+import { ThemeProvider } from "@material-ui/styles";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+  <ThemeProvider theme={createMuiTheme()}>
+    <App />
+  </ThemeProvider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
